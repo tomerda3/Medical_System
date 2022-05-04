3ac10854eafe4cbc1142c17a662dc545e3e495ff
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-05-04 13:59:23 +0300

    Create Medical.cs

---
+++ FinalProDoctor/Medical.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProDoctor
+{
+    public partial class Medical : Form
+    {
+        public Medical()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Medical_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
