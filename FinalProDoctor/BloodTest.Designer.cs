cef69ede40a43e73c7a31f9e539b224779b459b1
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-05-21 14:55:40 +0300

    Create BloodTest.Designer.cs

---
+++ FinalProDoctor/BloodTest.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace FinalProDoctor
+{
+    partial class BloodTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.APBox = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.HDLBox = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.IronBox = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.CrtnBox = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.HbBox = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.UreaBox = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.HCTBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.RBCBox = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.LymphBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.NeutBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.WBCBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // APBox
+            // 
+            this.APBox.Location = new System.Drawing.Point(168, 630);
+            this.APBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.APBox.Multiline = true;
+            this.APBox.Name = "APBox";
+            this.APBox.Size = new System.Drawing.Size(80, 36);
+            this.APBox.TabIndex = 91;
+            this.APBox.TextChanged += new System.EventHandler(this.APBox_TextChanged);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label20.Location = new System.Drawing.Point(80, 637);
+            this.label20.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(56, 29);
+            this.label20.TabIndex = 92;
+            this.label20.Text = "AP :";
+            // 
+            // HDLBox
+            // 
+            this.HDLBox.Location = new System.Drawing.Point(168, 574);
+            this.HDLBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HDLBox.Multiline = true;
+            this.HDLBox.Name = "HDLBox";
+            this.HDLBox.Size = new System.Drawing.Size(82, 36);
+            this.HDLBox.TabIndex = 89;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label19.Location = new System.Drawing.Point(80, 581);
+            this.label19.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(72, 29);
+            this.label19.TabIndex = 90;
+            this.label19.Text = "HDL :";
+            // 
+            // IronBox
+            // 
+            this.IronBox.Location = new System.Drawing.Point(168, 517);
+            this.IronBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.IronBox.Multiline = true;
+            this.IronBox.Name = "IronBox";
+            this.IronBox.Size = new System.Drawing.Size(80, 36);
+            this.IronBox.TabIndex = 87;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label17.Location = new System.Drawing.Point(78, 524);
+            this.label17.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(66, 29);
+            this.label17.TabIndex = 88;
+            this.label17.Text = "Iron :";
+            // 
+            // CrtnBox
+            // 
+            this.CrtnBox.Location = new System.Drawing.Point(168, 460);
+            this.CrtnBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.CrtnBox.Multiline = true;
+            this.CrtnBox.Name = "CrtnBox";
+            this.CrtnBox.Size = new System.Drawing.Size(80, 36);
+            this.CrtnBox.TabIndex = 85;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label14.Location = new System.Drawing.Point(78, 466);
+            this.label14.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(69, 29);
+            this.label14.TabIndex = 86;
+            this.label14.Text = "Crtn :";
+            // 
+            // HbBox
+            // 
+            this.HbBox.Location = new System.Drawing.Point(168, 398);
+            this.HbBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HbBox.Multiline = true;
+            this.HbBox.Name = "HbBox";
+            this.HbBox.Size = new System.Drawing.Size(80, 36);
+            this.HbBox.TabIndex = 83;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label15.Location = new System.Drawing.Point(78, 404);
+            this.label15.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(56, 29);
+            this.label15.TabIndex = 84;
+            this.label15.Text = "Hb :";
+            // 
+            // UreaBox
+            // 
+            this.UreaBox.Location = new System.Drawing.Point(168, 343);
+            this.UreaBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UreaBox.Multiline = true;
+            this.UreaBox.Name = "UreaBox";
+            this.UreaBox.Size = new System.Drawing.Size(80, 36);
+            this.UreaBox.TabIndex = 81;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label16.Location = new System.Drawing.Point(78, 350);
+            this.label16.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(77, 29);
+            this.label16.TabIndex = 82;
+            this.label16.Text = "Urea :";
+            // 
+            // HCTBox
+            // 
+            this.HCTBox.Location = new System.Drawing.Point(168, 281);
+            this.HCTBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HCTBox.Multiline = true;
+            this.HCTBox.Name = "HCTBox";
+            this.HCTBox.Size = new System.Drawing.Size(80, 36);
+            this.HCTBox.TabIndex = 79;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label12.Location = new System.Drawing.Point(78, 289);
+            this.label12.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 29);
+            this.label12.TabIndex = 80;
+            this.label12.Text = "HCT :";
+            // 
+            // RBCBox
+            // 
+            this.RBCBox.Location = new System.Drawing.Point(168, 221);
+            this.RBCBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.RBCBox.Multiline = true;
+            this.RBCBox.Name = "RBCBox";
+            this.RBCBox.Size = new System.Drawing.Size(80, 36);
+            this.RBCBox.TabIndex = 77;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label13.Location = new System.Drawing.Point(78, 227);
+            this.label13.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(75, 29);
+            this.label13.TabIndex = 78;
+            this.label13.Text = "RBC :";
+            // 
+            // LymphBox
+            // 
+            this.LymphBox.Location = new System.Drawing.Point(168, 166);
+            this.LymphBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LymphBox.Multiline = true;
+            this.LymphBox.Name = "LymphBox";
+            this.LymphBox.Size = new System.Drawing.Size(80, 36);
+            this.LymphBox.TabIndex = 75;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label11.Location = new System.Drawing.Point(78, 174);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(96, 29);
+            this.label11.TabIndex = 76;
+            this.label11.Text = "Lymph :";
+            // 
+            // NeutBox
+            // 
+            this.NeutBox.Location = new System.Drawing.Point(168, 104);
+            this.NeutBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.NeutBox.Multiline = true;
+            this.NeutBox.Name = "NeutBox";
+            this.NeutBox.Size = new System.Drawing.Size(80, 36);
+            this.NeutBox.TabIndex = 73;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label10.Location = new System.Drawing.Point(78, 112);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 29);
+            this.label10.TabIndex = 74;
+            this.label10.Text = "Neut : ";
+            // 
+            // WBCBox
+            // 
+            this.WBCBox.Location = new System.Drawing.Point(168, 46);
+            this.WBCBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.WBCBox.Multiline = true;
+            this.WBCBox.Name = "WBCBox";
+            this.WBCBox.Size = new System.Drawing.Size(80, 36);
+            this.WBCBox.TabIndex = 71;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label9.Location = new System.Drawing.Point(78, 54);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 29);
+            this.label9.TabIndex = 72;
+            this.label9.Text = "WBC :";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button3.Location = new System.Drawing.Point(85, 732);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 54);
+            this.button3.TabIndex = 93;
+            this.button3.Text = "הזן";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(85, 695);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 26);
+            this.textBox3.TabIndex = 94;
+            // 
+            // BloodTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 816);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.APBox);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.HDLBox);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.IronBox);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.CrtnBox);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.HbBox);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.UreaBox);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.HCTBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.RBCBox);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.LymphBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.NeutBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.WBCBox);
+            this.Controls.Add(this.label9);
+            this.Name = "BloodTest";
+            this.Text = "BloodTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox APBox;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox HDLBox;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox IronBox;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox CrtnBox;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox HbBox;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox UreaBox;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox HCTBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox RBCBox;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox LymphBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox NeutBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox WBCBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
