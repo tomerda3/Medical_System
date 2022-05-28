1cbf62dced32cd0c695a81ffcb7a01146dc034e7
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-05-28 19:05:31 +0300

    Update BloodTest.cs

--- FinalProDoctor/BloodTest.cs
+++ FinalProDoctor/BloodTest.cs
@@ -23,25 +23,21 @@ namespace FinalProDoctor
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            float WBC = 0;
-            float Neut = 0;
-            float Lymph = 0;
-            float RBC = 0;
-            float HCT = 0;
-            float Urea = 0;
-            float Hb = 0;
-            float Crtn = 0;
-            float Iron = 0;
-            float HDL = 0;
-            float AP = 0;
+            float WBC =0;
+            float Neut =0;
+            float Lymph =0;
+            float RBC =0;
+            float HCT =0;
+            float Urea =0;
+            float Hb =0;
+            float Crtn =0;
+            float Iron =0;
+            float HDL =0;
+            float AP =0;
 
             try
             {
-                // if
-                // input blood test from excel.
 
-                // else
-                // input blood test from user.
                 WBC = float.Parse(WBCBox.Text);
                 Neut = float.Parse(NeutBox.Text);
                 Lymph = float.Parse(LymphBox.Text);
@@ -59,6 +55,7 @@ namespace FinalProDoctor
             catch (FormatException e1)
             {
                 textBox3.Text = "There is a problem in the input data";
+                return;
             }
 
             Medical.instance.setBlood(WBC, Neut, Lymph, RBC, HCT, Urea, Hb, Crtn, Iron, HDL, AP);
